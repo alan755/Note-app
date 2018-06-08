6a8c6854132b16f1b834355da0ae243ec8f0e03f
alanmathew333 <mathewalan84@gmail.com>
2018-06-08 14:45:33 +0530

    Modified app.js and notes.js

--- src/app.js
+++ src/app.js
@@ -7,12 +7,25 @@ const notes = require('./notes.js')
 
 
 var argv = yargs.argv
-var command = process.argv[2]
+var command = argv._[0]
+
 if (command === 'add') {
-  notes.add(argv.title,argv.body)
-} else if(command === 'list') {
-console.log('Not yet done')
+  console.log('Command: add')
+  var note = notes.add(argv.title,argv.body)
+  if (note) {
+    console.log('Note added successfully...!')
+  } else {
+    console.log('Title is already taken...!')
+  }
+
+} else if (command === 'list') {
+  console.log('Command: list ')
+  notes.list()
+} else if (command === 'remove') {
+  console.log('command: remove')
+  notes.remove(argv.title) 
+  console.log('Note with the specified title is removed..!')
 } else {
-console.log('Command not recognized')
+  console.log('Command not recognized')
 }
 
--- src/notes.js
+++ src/notes.js
@@ -1,8 +1,53 @@
-var add = (title, body) => {
-  console.log('Adding notes..\n',title,'\n',body)
+const fs = require('fs')
+
+var n = []
+var singlenote = {
+  title,
+  body
 }
 
+var fetchnotes = () => {
+  try {
+    var notestring = fs.readFileSync('note-data.json')
+    n = JSON.parse(notestring)
+    return n
+  
+  } catch (e) {
+    return []
+  }
+}
+var savenotes = (n) => {
+  fs.writeFileSync('note-data.json', JSON.stringify(n))
+}
+
+var add = (title, body) => {
+
+
+  var duplicate = n.filter((singlenote) => singlenote.title === title)
+  if (duplicate.length === 0) {
+    n.push(singlenote)
+    savenotes(n)
+    return singlenote
+  }
+
+}
+
+var list = () => {
+  var not = JSON.parse(fs.readFileSync('note-data.json'))
+  console.log('The list of notes are:-\n')
+  console.log(n)
+}
+
+var remove = (title) => {
+    var not = fetchnotes()
+    var newnotes = not.filter((singlenote) => singlenote.title !==title)
+    savenotes(newnotes)
+  }
+  
+
 module.exports = {
-  add
+  add,
+  list,
+  remove
 }
 
